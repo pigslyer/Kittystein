8888eccd0071bc3b5c725d68eca6f5d22f6857af
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-05-21 23:26:00 +0200

    linked list unit test

--- src/milk/m_test.c
+++ src/milk/m_test.c
@@ -1,5 +1,7 @@
 #include "m_internal.h"
 
+#include <stdlib.h>
+
 #define FLOOFY_EXPOSE_TEST_SUITE
 #include <floofy.h>
 
@@ -16,18 +18,53 @@ void linked_list_test()
 	LinkedList* list = milk_linked_list_new(sizeof *testValues);
 
 	tempInt = milk_linked_list_count(list);
-	FLOOFY_TEST_ASSERT(tempInt == 0, "Linked list count of new list is %d, instead of 0!", tempInt);
+	FLOOFY_TEST_ASSERT_ARG(tempInt == 0, "Linked list count of new list is %d, instead of 0!", tempInt);
 
 	milk_linked_list_add_first(list, &testValues[0]);
 
 	tempInt = milk_linked_list_count(list);
-	FLOOFY_TEST_ASSERT(tempInt == 1, "Linked list count of list with 1 element is %d, instead of 1!", tempInt);
+	FLOOFY_TEST_ASSERT_ARG(tempInt == 1, "Linked list count of list with 1 element is %d, instead of 1!", tempInt);
 
 	tempNode = milk_linked_list_iterate(list);
 	tempIntPtr = (int*)milk_linked_list_node_value(tempNode);
-	FLOOFY_TEST_ASSERT(*tempIntPtr == testValues[0], "Linked list recorded value %d instead of %d!", tempInt, testValues[0]);
+	FLOOFY_TEST_ASSERT_ARG(*tempIntPtr == testValues[0], "Linked list recorded value %d instead of %d!", tempInt, testValues[0]);
 
 	*tempIntPtr = testValues[1];
 	tempIntPtr = (int*) milk_linked_list_node_value(tempNode);
-	FLOOFY_TEST_ASSERT(*tempIntPtr == testValues[1], "Linked list node value did not change despite setting pointer! Current: %d, Expected: %d!", *tempIntPtr, testValues[1]);
+	FLOOFY_TEST_ASSERT_ARG(*tempIntPtr == testValues[1], "Linked list node value did not change despite setting pointer! Current: %d, Expected: %d!", *tempIntPtr, testValues[1]);
+
+	milk_linked_list_free(list);
+
+	list = milk_linked_list_new(sizeof *testValues);
+	const int listLength = sizeof testValues / sizeof *testValues;
+
+	for (uint i = 0; i < listLength; i++)
+	{
+		milk_linked_list_add_first(list, &testValues[i]);
+	}
+
+	tempNode = milk_linked_list_iterate(list);
+
+	for (int i = listLength - 1; i >= 0; i--)
+	{
+		FLOOFY_TEST_ASSERT_ARG(tempNode != null, "Ran out of nodes at index %d!", i);
+		
+		tempInt = *(int*)milk_linked_list_node_value(tempNode);
+		
+		FLOOFY_TEST_ASSERT_ARG(tempInt == testValues[i], "Reading linked list in reverse adding order doesn't yield equal values at index %d; read value: %d, expected value: %d!", i, tempInt, testValues[i]);
+		
+		tempNode = milk_linked_list_node_next(tempNode);
+	}
+
+	FLOOFY_TEST_ASSERT(tempNode == null, "Too many nodes in list!");
+
+	int* arr = (int*) milk_linked_list_to_array(list);
+
+	for (uint i = 0; i < listLength; i++)
+	{
+		FLOOFY_TEST_ASSERT_ARG(arr[i] == testValues[listLength - 1 - i], "Values in array aren't equal! Array: %d, Original: %d!", arr[i], testValues[listLength - 1 - i]);
+	}
+
+	free(arr);
+	milk_linked_list_free(list);
 }
