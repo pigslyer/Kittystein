a7c90c2ad005b08dc0b42a29316bc5f7b5513fb6
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-05-21 19:50:53 +0200

    better documentation for (mostly finished) modules.

--- include/floofy.h
+++ include/floofy.h
@@ -1,3 +1,15 @@
+/*
+* Floofy is a unit testing module which depends upon Watchman.
+* Functions can be registered to Floofy via the FLOOFY_TEST_REGISTER function, whereupon 
+* (assuming __attribute__((constructor)) is supported by the compiler), they will be added on startup to an internal list
+* of functions which can be iterated through via floofy_test_run(). 
+*
+* Tests should use floofy_test_assert in order to test whether their conditions are true. If they are not, their formatted error 
+* message will be outputted via a call to watchman_log_error().
+* 
+* Use #include FLOOFY_EXPOSE_TEST_SUITE to expose internal #defines and functions.
+* */
+
 #ifndef FLOOFY_H
 #define FLOOFY_H
 
@@ -9,15 +21,45 @@ typedef void (*FloofyTestFunction)(void);
 
 #endif
 
+/// @brief Runs all registered tests, outputting the number of tests run and any failed assertions found along the way.
 void floofy_test_run(void);
+
+
+#ifdef FLOOFY_EXPOSE_TEST_SUITE
+
+/// @brief Registers function to Floofy as runnable test.
+/// @param func The new test.
 void floofy_test_function(FloofyTestFunction func);
-void floofy_test_assert(bool value, const char* const message, ...);
 
-#define FLOOFY_REGISTER_TEST(name, func)								\
-	static void name##_function(void);									\
-	void name##_function() func											\
-	static void name##_register(void) __attribute__((constructor));		\
-	void name##_register(void) { floofy_test_function(name##_function); }
+/// @brief Registers function to Floofy as runnable test. This variant is automatically called by FLOOFY_TEST_REGISTER.
+/// @param func The new test.
+/// @param file The file in which the test is defined.
+/// @param line The line on which the test is started.
+void floofy_test_function_full(FloofyTestFunction func, char* file, uint line);
 
+/// @brief Checks whether condition is false, printing the formatted message to Floofy's output.
+/// @param condition The condition we're checking against.
+/// @param error The formatted message to be outputted if the condition is FALSE.
+void floofy_test_assert(bool condition, const char* const error, ...);
+
+/// @brief Checks whether condition is false, printing the formatted message to Floofy's output. This function is called automatically
+/// by FLOOFY_TEST_ASSERT.
+/// @param file The file this assertion failed in.
+/// @param line The line on which this assertion failed.
+/// @param condition The condition we're checking against.
+/// @param error The formatted message to be outputted if the condition is FALSE.
+void floofy_test_assert_full(char* file, uint line, bool value, const char* const error, ...);
+
+
+#define FLOOFY_TEST_REGISTER(name, func)														\
+	static void name##_function(void);															\
+	void name##_function() func																	\
+	static void name##_register(void) __attribute__((constructor));								\
+	void name##_register(void) { floofy_test_function_full(name##_function, __FILE__, __LINE__); }
+
+#define FLOOFY_TEST_ASSERT(condition, error, ...)									\
+	floofy_test_assert_full(__FILE__, __LINE__, condition, error, __VA_ARGS__)
+
+#endif
 
 #endif
--- include/watchman.h
+++ include/watchman.h
@@ -1,14 +1,44 @@
+/*
+* A logging module, which allows printing formatted errors, warnings and messages.
+*
+* By default outputs to stdout, this can be overriden via a call to watchman_stream_set_out().
+* 
+* In the case that, instead of variadic arguments, a va_list parameter is required, use #include WATCHMAN_EXPOSE_VA_LIST
+* to expose log_v variants of the 3 log methods.
+* */
+
 #ifndef WATCHMAN_H
 #define WATCHMAN_H
 
 #include <stdarg.h>
 
+/// @brief Logs error to watchman.
+/// @param message The intended error message.
 void watchman_log_error(const char* message, ...);
+
+/// @brief Logs warning to watchman.
+/// @param message The intended warning message.
 void watchman_log_warning(const char* message, ...);
+
+/// @brief Logs message to watchman.
+/// @param message The intended message.
 void watchman_log_message(const char* message, ...);
 
+
+#ifdef WATCHMAN_EXPOSE_VA_LIST
+
+/// @brief Logs error to watchman.
+/// @param message The intended error message.
 void watchman_log_errorv(const char* message, va_list list);
+
+/// @brief Logs warning to watchman.
+/// @param message The intended warning message.
 void watchman_log_warningv(const char* message, va_list list);
+
+/// @brief Logs message to watchman.
+/// @param message The intended message.
 void watchman_log_messagev(const char* message, va_list list);
+
+#endif
 
 #endif
