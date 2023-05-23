8493d49e85193366427bc93e600a19a83f768ccf
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-05-23 17:29:05 +0200

    added qualifiers to pathless directory opening functions

--- include/pathless.h
+++ include/pathless.h
@@ -1,3 +1,11 @@
+/*
+* Pathless is a module dependant on Milk which implements various file I/O operations.
+* 
+* 
+* 
+* Named after the Pathless (https://store.steampowered.com/app/1492680/The_Pathless/)
+* */
+
 #ifndef PATHLESS_H
 #define PATHLESS_H
 
@@ -11,14 +19,14 @@ typedef void Directory;
 #endif
 
 
-IterationFile* pathless_iterate_begin(char* path, bool recursive, char** excludeExtensions);
+IterationFile* pathless_iterate_begin(const char* const path, bool recursive, char** excludeExtensions);
 void pathless_iterate_end(IterationFile* state);
 
 char* pathless_iterate_get_path(IterationFile* state);
 bool pathless_iterate_has_next(IterationFile* state);
 bool pathless_iterate_is_directory(IterationFile* state);
 
-Directory* pathless_directory_open(char* path);
+Directory* pathless_directory_open(const char* const path);
 void pathless_directory_close(Directory* directory);
 
 #endif
