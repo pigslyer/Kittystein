fb8bbe1bad9adb0d8cdaada1c224dc53d63c60b2
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-05-25 17:38:07 +0200

    implementation of memory allocation struct

--- src/delight/d_internal.h
+++ src/delight/d_internal.h
@@ -1,7 +1,15 @@
 #ifndef DELIGHT_INTERNAL
 #define DELIGHT_INTERNAL
 
+#include <stdint.h>
 
+typedef struct
+{
+	void* allocationLocation;
+	size_t allocationAmount;
+	char* requestLocation;
+	unsigned int requestLine;
+} MemoryAllocation;
 
 #include <delight.h>
 
