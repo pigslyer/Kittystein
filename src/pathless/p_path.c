f7da400151dcea2a97bb3fae73022d5f3d38bbc6
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-05-27 10:48:00 +0200

    is_dir bug fixed

--- src/pathless/p_path.c
+++ src/pathless/p_path.c
@@ -4,5 +4,5 @@ bool pathless_path_is_dir(const char* const path)
 {
 	size_t len = delight_string_length(path);
 
-	return path[len - 1] == '.' || path[len - 2] == '.' || path[len - 3] == '.' || path[len - 4] == '.';
+	return !(path[len - 1] == '.' || path[len - 2] == '.' || path[len - 3] == '.' || path[len - 4] == '.');
 }
