3e1560c421b54945aa4a844bd9df5a2293346cd3
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-06-11 18:24:59 +0200

    fixed missing delight inclusion

--- src/witness/w_internal.h
+++ src/witness/w_internal.h
@@ -1,9 +1,11 @@
 #ifndef WITNESS_INTERNAL
 #define WITNESS_INTERNAL
 
+#include <delight.h>
+
 char* witness_input_get_line_base(void);
 
-char* witness_input_get_line();
+char* witness_input_get_line(void);
 
 uint witness_input_get_command(char*** params);
 
